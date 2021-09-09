7fc5c456daa3c79b0973bc10368c67ad4fe686ee
Bruno Miquelini Dos Santos <bsmiquelini@cast.fazenda.sp.gov.br>
2021-09-09 16:00:02 +0000

    Updated entrypoint.sh

--- modelo_core_webapi/docker/entrypoint.sh
+++ modelo_core_webapi/docker/entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-
 #Script de Entrypoint, que fará a chamada da aplicação Dotnet e renovação de Ticket Kerberos
-
 #Informar o arquivo de DLL que deverá ser carregado
 DLL="modelo_core_webapi.dll"
-
 #Script de renovação de Ticket
 sh krenew.sh
-
 #Chamada do executavel da Aplicação
 dotnet $DLL
