b757390798f621f53c149658753f9939b60fed8f
Walter de Tarso <wtfscampos@fazenda.sp.gov.br>
2021-12-30 10:47:08 -0300

    Remoção de arquivos desnecessários

--- EntityFramework/Modelo_Core_Entity_Webapi_old/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Modelo.Core.ADO.WebApi.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
